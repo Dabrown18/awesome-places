7fe1628c81756c0e32e8da6d6592c195837244f7
Darron Brown <DarronBrown88@outlook.com>
2018-08-25 09:46:53 -0600

    middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,8 +6,14 @@ const rootReducer = combineReducers({
 	places: placesReducer
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+	composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-	return createStore(rootReducer)
+	return createStore(rootReducer, composeEnhancers())
 };
 
 export default configureStore;
