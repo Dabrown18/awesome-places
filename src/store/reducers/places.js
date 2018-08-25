e3e061803737054a9fa377d39e402fefa67e0c7d
Darron Brown <DarronBrown88@outlook.com>
2018-08-24 20:48:00 -0600

    add place case

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -7,6 +7,18 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 	switch(action.type) {
+		case ADD_PLACE:
+			return {
+				// Copies properties of the old state
+				...state,
+				places: prevState.places.concat({
+					key: Math.random(),
+					name: action.placeName,
+					image: {
+						uri: "https://media.istockphoto.com/photos/paradise-beach-picture-id509488176?k=6&m=509488176&s=612x612&w=0&h=xUkao6mVjcKAAQNnf0aB5kKEfvg98pl1QzTSvSi-8PE="
+					}
+				})
+			};
 		default:
 			return state;
 	}
