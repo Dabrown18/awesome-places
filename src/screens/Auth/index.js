d530a0e0dce1efad5b85e32dde858f0d1d7c612f
Darron Brown <DarronBrown88@outlook.com>
2018-08-27 16:12:12 -0600

    added button to auth screen

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 export default class Auth extends Component {
+	loginHandler = () => {
+
+	};
+
 	render() {
 		return (
 			<View>
 				<Text>AuthSreen</Text>
+				<Button title="Login" onPress={this.loginHandler} />
 			</View>
 		)
 	}
