511be8c0a1dfbe536cccf134846dc928f7ef8e9d
Darron Brown <DarronBrown88@outlook.com>
2018-09-26 15:00:05 -0600

    setup MainNavigation

--- src/screens/Main/startMainTabs.js
+++ src/screens/Main/startMainTabs.js
@@ -1,1 +1,18 @@
+import { Navigation } from 'react-native-navigation';
 
+export const startTabs = () => {
+	Navigation.startTabBasedApp({
+		tabs: [
+			{
+				screen: "awesome-places.FindPlaceScreen",
+				label:  "Find Place",
+				title:  "Find Place"
+			},
+			{
+				screen: "awesome-places.SharePlaceScreen",
+				label:  "Share Place",
+				title:  "Share Place"
+			}
+		]
+	});
+};
